92ac71448fac36bb67745b58de4cc754444d7b40
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-06-23 23:34:18 +0530

    Done

---
+++ googledoc/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Editer from './Components/Googledoc/Editer';
+import Navbar from './Components/Navbar/Navbar';
+
+
+
+function App() {
+
+ 
+  return (
+    <div className="App">
+      <Navbar/>
+      <Editer/>
+    </div>
+  );
+}
+
+export default App;
